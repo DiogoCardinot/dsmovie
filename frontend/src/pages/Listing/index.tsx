b6576aa17b90fd2cc5fe838203d5cd3c6263228f
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-01-15 12:30:28 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,60 +7,47 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-      const data = response.data as MoviePage;
-      console.log(data);
-      setPageNumber(data.number);
-    });
-  }, []);
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
   //Deixando o segundo argumento do useEffect vazio ele só roda a função quando carregar a página
 
   return (
     <>
-      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
         {/* Essa classe container é do bootstrap, ela tem estilização própria e nos ajuda*/}
         <div className="row">
-          {/* Essa classe row é do bootstrap, ela tem estilização própria e nos ajuda*/}
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
-            {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
-            {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
-            {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
-            {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
-            {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
-            {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
-            {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
-            {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
-            {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
-            {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
-            {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
-            {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
-            {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
-            {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
-            {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => {
+            return (
+              //Essa classe row é do bootstrap, ela tem estilização própria e nos ajuda
+              <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                {/* col-tamanhoDeTela-quantoVaiOcupar das 12 colunas disponíveis*/}
+                {/* col-lg-4 cada card vai ocupar 4colunas, ou seja, teremos 3 cards a cada linha para telas large (>= 992px)*/}
+                {/*https://getbootstrap.com/docs/4.0/layout/grid/ */}
+                {/* Essa classe column é do bootstrap, ela tem estilização própria e nos ajuda*/}
+                <MovieCard movie={movie} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
