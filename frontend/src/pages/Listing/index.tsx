1711b194379d94d01a15e7869287d7c8b99aa223
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-01-15 12:15:12 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,25 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-  //FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-    console.log(response.data);
-  });
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
+  //Deixando o segundo argumento do useEffect vazio ele só roda a função quando carregar a página
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
